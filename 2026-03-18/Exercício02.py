539f95fe748002fa2490de604d5fee1447bb3408
Evelyn-Senpai <128evelynaraujo@gmail.com>
2026-03-19 15:58:46 -0300

    Não feito

---
+++ 2026-03-18/Exercício02.py
@@ -0,0 +1,6 @@
+'''
+Diremos que um número de n dígitos é pandigital se ele utiliza todos os dígitos de 1 
+até n exatamente uma vez. 
+Por exemplo, 2143 é um número pandigital de 4 dígitos e também é primo. 
+Qual é o maior número primo pandigital de n dígitos que existe?
+'''
