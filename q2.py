4ab10a8a1b857ad20f6cb2a21612bd040c6b860d
Evelyn-Senpai <128evelynaraujo@gmail.com>
2026-04-15 15:07:12 -0300

    feito

--- q2.py
+++ q2.py
@@ -7,6 +7,6 @@ while True:
     if (int.from_bytes(hash, 'big') >> (256 - num)) == 0: # Primeiro: Converte o hash em um número inteiro, Segundo: O hash tem 256 bits, então é subtraido 256 de num (a quantidade de zeros desejado), o resultado são os primeiro bits do hash que são num. Se isso for verdade, é encontrado o nonce do hash.
         print('Nonce encontrado: ', nonce) # print do nonce.
         print('Hash: ', hash) # print do hash.
-        break
+        break # Para a execução
     else: # Se não é igual.
         nonce += 1  # O nonce é acrescido mais 1.
