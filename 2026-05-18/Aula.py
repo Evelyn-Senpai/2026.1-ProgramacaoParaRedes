ff9c441003400d9ca1a9ec0e81d4ceab20515625
Evelyn-Senpai <128evelynaraujo@gmail.com>
2026-05-18 20:17:58 -0300

    Codificação base64

--- 2026-05-18/Aula.py
+++ 2026-05-18/Aula.py
@@ -1,6 +1,6 @@
 import base64
 # Texto a ser codificado
-texto = "Olá, mundo!"
+texto = "Olá mundo!"
 # Codificação para Base64
 texto_codificado = base64.b64encode(texto.encode('utf-8'))
 print("Texto codificado:", texto_codificado.decode('utf-8'))
