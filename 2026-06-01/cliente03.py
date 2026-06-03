8569e0e60059ed14bee76b4cdacdc3ab9bf862f0
Evelyn-Senpai <128evelynaraujo@gmail.com>
2026-06-03 16:37:43 -0300

    Feito

--- 2026-06-01/cliente03.py
+++ 2026-06-01/cliente03.py
@@ -2,21 +2,21 @@ import socket
 from config import *
 
 my_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+my_sock.settimeout(1)
 
-server_ip = input("IP/nome do servidor: ")
+server_ip = input ("IP/nome do servidor: ")
 
 msg = b''
 while msg != END:
-    msg = input('Mensagem: ').encode()
-
+    msg = input("Mensagem: ").encode()
     if msg:
-        print(f'Enviado: {msg}')
-
+        print (f"Enviando: {msg}")
         my_sock.sendto(msg, (server_ip, PORT))
-
-    answer, source = my_sock.recvfrom(512)
-
-    print(f'Recebido de {source}: {answer}')
-
-print(f'Digitado {END}. Cliente encerrado.')
+    try:
+        answer, source = my_sock.recvfrom(512)
+        print (f"Recebido de {source}: {answer}")
+    except socket.timeout as e:
+        print (f"Sem mensagem a receber")
+        None
+print (f"Digitado {END}. Saindo.")
 my_sock.close()
--- 2026-06-01/servidor.py
+++ 2026-06-01/servidor.py
@@ -3,27 +3,24 @@ from config import *
 
 def get_my_ip10():
     return [addr[4][0] 
-        for addr in socket.getaddrinfo(socket.gethostname(), 80) 
-            if addr[4][0].startswith('10.')][0]
+         for addr in socket.getaddrinfo(socket.gethostname(), 80) 
+             if addr[4][0].startswith('10.')
+        ][0]
 
 my_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_ip = get_my_ip10()
+users = set()
 
-print(f'Escutando em ({my_ip}:{PORT})')
-
-my_sock.bind((my_ip, PORT))
-
-clientes = set()
+print (f"Escutando em ({my_ip}:{PORT})")
+my_sock.bind ((my_ip, PORT))
 
 msg = b''
 while msg != END:
     msg, source = my_sock.recvfrom(512)
-
-    clientes.update({source})
-
-    for cliente in clientes:
-        if cliente != source:
-            my_sock.sendto(msg, source)
-
-print(f'Recebi {END}. Servidor encerrado.')    
+    users.update([source])
+    for user in users:
+        if user != source:
+            my_sock.sendto(msg, user)
+            
+print (f"Recebi {END}. Saindo.")
 my_sock.close()
