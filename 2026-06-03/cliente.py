d53addd11e76ca5c7e16277d46a5caf8c5c88ae1
Evelyn-Senpai <128evelynaraujo@gmail.com>
2026-06-03 20:32:42 -0300

    Não terminado

--- 2026-06-03/cliente.py
+++ 2026-06-03/cliente.py
@@ -6,17 +6,15 @@ my_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
     file_name = input("Nome do arquivo a baixar: ")
-    if file_name == "Sair":
-        my_sock.sendto(b'Sair', SERVER)
-        print('Cliente encerrado.')
-        break
     my_sock.sendto(file_name.encode(), SERVER)
-
     fd = open (CLIENT_FILES+file_name, "wb")
-    while True:
-        data, source = my_sock.recvfrom(1024)
-        if data == b'Sair':
-            break
+    tamanho, source = my_sock.recvfrom(4)
+    tamanho = int.from_bytes(tamanho, 'big')
+    data, source = my_sock.recvfrom(1024)
+    total = len(data)
+    while total < tamanho:
         fd.write(data)
+        data, source = my_sock.recvfrom(1024)
+        total += len(data)
     fd.close()
     print('Download concluído.')
--- 2026-06-03/servidor.py
+++ 2026-06-03/servidor.py
@@ -7,16 +7,16 @@ my_sock.bind (('127.0.0.1', 12346))
 while True:
     # Le do cliente o nome do arquivo
     file_name, source = my_sock.recvfrom(512)
-    if file_name == b'Sair':
-        print('Servidor encerrado.')
-        break
     fd = open (SERVER_FILES+file_name.decode(), "rb")
-    while True:
-        bloco = fd.read(1024)
-        if not bloco:
-            break
-        my_sock.sendto(bloco, source)
-    fd.close()
-
-    my_sock.sendto(b'Sair', source)
-my_sock.close()
+    # Indo para o final do arquivo
+    pos_final = fd.seek(0, 2)
+    tamanho =  pos_final.to_bytes(4, 'big')
+    my_sock.sendto(tamanho, source)
+    # Voltando para o inicio do arquivo
+    fd.seek(0, 0)
+    data = fd.read(1024)
+    total = 0
+    while data != b'':
+        my_sock.sendto(data, source)
+        total += len(data)
+        my_sock.close()
