a5bf455e8a391579ddc2946d34ac1cc212d4a9a7
Evelyn-Senpai <128evelynaraujo@gmail.com>
2026-06-17 19:54:16 -0300

    p

--- Projetos/pr2026-1.py
+++ Projetos/pr2026-1.py
@@ -1,1 +1,12 @@
-'''Quebrador de senhas'''
+'''Quebrador de senhas distribuido'''
+
+'''
+Servidor, conversa com um humano, pergunta: qual o hash da senha a quebrar?, a pessoa responde e o servidor tentar quabrar a senha por força bruta.
+Ess servidor tem uma interface com o telegram
+O humano manda para o telegram e o telegram manda para o servidor.
+o servidor não quebra senhas
+Tem qubrador1, que faz: tem alguma senha pra quebrar?, o servidor responde, e o servidor dá a senha para quebrar
+O servidor da a mesma senha para vários.
+O servidor diz: Quebre essa parte da senha.
+O servidor confere se a senha já foi quebrada e avisa todo mundo.
+'''
